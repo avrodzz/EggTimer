f7f925bb225d3402121a1df392b8c66cbfbc2e0f
Alexis Rodriguez <avrodzz@gmail.com>
2023-04-24 02:27:18 -0700

    Working on getting the timer to print out the seconds to the console.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,8 +8,55 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     
-
-
+    //soft egg - 5 mins
+    //medium egg - 8 mins
+    //hard egg - 12 mins
+    let eggTimes = ["Soft": 5, "Medium": 8, "Hard": 12]
+    
+    var counter = 60
+    var timer = Timer()
+    
+    @objc func updateCounter() {
+        //example functionality
+        if counter > 0 {
+            print("\(counter) seconds.")
+            counter -= 1
+        }
+        print("\(counter)")
+    }
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        let cookTime = eggTimes[hardness]!
+        print(cookTime)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: false)
+        
+        
+        
+//        if hardness == "Soft" {
+//            print(softTime)
+//        }
+//        else if hardness == "Medium" {
+//            print(mediumTime)
+//        }
+//        else {
+//            print(hardTime)
+//        }
+        
+//        // switch version
+//        switch hardness {
+//        case "Soft":
+//            print(softTime)
+//        case "Medium":
+//            print(mediumTime)
+//        case "Hard":
+//            print(hardTime)
+//        default:
+//            print("Error.")
+//        }
+    }
 }
